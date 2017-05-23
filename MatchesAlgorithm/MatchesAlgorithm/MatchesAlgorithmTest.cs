6ca3ee4112ceb769fe69207070aa8f8d07a9b5cb
TamasR <razvit88@gmail.com>
2017-05-23 04:44:44 -0700

    Name changes and changed the IsMatching function to a boolean one

--- MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
+++ MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
@@ -11,73 +11,81 @@ namespace MatchesAlgorithm
         public void FirstLetterTest()
         {
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
-            CollectionAssert.AreEqual(new[] { "Ovidiu Jurje" }, FindUserNamesWithMatchingLetters(userNameList, "O"));
+            CollectionAssert.AreEqual(new[] { "Ovidiu Jurje" }, FindUsersNamesWithMatchingLetters(userNameList, "O"));
         }
        
         [TestMethod]
         public void FirstTwoLettersOfFirstNameTest()
         {
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
-            CollectionAssert.AreEqual(new[] { "Razvan Tamas","Razvan Hidan" },FindUserNamesWithMatchingLetters(userNameList,"Ra"));
+            CollectionAssert.AreEqual(new[] { "Razvan Tamas","Razvan Hidan" },FindUsersNamesWithMatchingLetters(userNameList,"Ra"));
         }
         [TestMethod]
         public void LettersFromBothNamesTest()
         {
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
-            CollectionAssert.AreEqual(new[] { "Razvan Hidan" }, FindUserNamesWithMatchingLetters(userNameList, "RaHid"));
+            CollectionAssert.AreEqual(new[] { "Razvan Hidan" }, FindUsersNamesWithMatchingLetters(userNameList, "RaHid"));
         }
         [TestMethod]
         public void TestIfNoLettersMatch()
         {
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };           
-            CollectionAssert.AreEqual(new List<string>(), FindUserNamesWithMatchingLetters(userNameList, "PoTo"));
+            CollectionAssert.AreEqual(new List<string>(), FindUsersNamesWithMatchingLetters(userNameList, "PoTo"));
         }
         [TestMethod]
         public void TestIfOnlyFewLettersMatch()
         {
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
-            CollectionAssert.AreEqual(new List<string>(), FindUserNamesWithMatchingLetters(userNameList, "RaJu"));
+            CollectionAssert.AreEqual(new List<string>(), FindUsersNamesWithMatchingLetters(userNameList, "RaJu"));
         }
         [TestMethod]
         public void TestIfAllLettersAreMatchedInTheSecondName()
         {
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
-            CollectionAssert.AreEqual(new[] { "Ovidiu Jurje" }, FindUserNamesWithMatchingLetters(userNameList, "Ju"));
+            CollectionAssert.AreEqual(new[] { "Ovidiu Jurje" }, FindUsersNamesWithMatchingLetters(userNameList, "Ju"));
+        }
+        [TestMethod]
+        public void TestFor()
+        {
+
         }
        
-        List<string> FindUserNamesWithMatchingLetters(List<string> userNameList, string searchLetters)
+        List<string> FindUsersNamesWithMatchingLetters(List<string> userNameList, string searchLetters)
         {
             var nameResults = new List<string>();
             foreach (var userName in userNameList)
             {
-
-                GoThroughUsers(searchLetters, nameResults, userName);
+                if (IsMatching(searchLetters, userName))
+                    nameResults.Add(userName);
             }
             return nameResults;
         }
 
-        public void GoThroughUsers(string searchLetters, List<string> results, string userName)
+        public bool IsMatching(string searchLetters, string text)
         {
-            var userNamesInArray=userName.Split(' ');
-            
+            var parts = text.Split(' ');           
             int i = 0;
             int j = 0;
-            int nameNumber = 0;
+            int partIndex = 0;
             while (i < searchLetters.Length)
             {
-                if (searchLetters[i] == userNamesInArray[nameNumber][j])
+                if (searchLetters[i] == parts[partIndex][j])
                 {
-                    if (i == searchLetters.Length - 1) results.Add(userName);
+                    if (i == searchLetters.Length - 1)
+                    {
+                        return true;
+                    }                    
                     i++;
                     j++;
                 }
-                else if (nameNumber + 1 < userNamesInArray.Length)
+                else if (partIndex + 1 < parts.Length)
                 {
-                    nameNumber++;
-                    j = 0;
+                    partIndex++;
+                    j = 0;                  
                 }
                 else break;
             }
+            return false;
         }
 
        
