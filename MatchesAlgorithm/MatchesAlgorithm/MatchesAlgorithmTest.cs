9da9861a95df58ccd03940419b551c459ac39f52
TamasR <razvit88@gmail.com>
2017-05-23 02:40:06 -0700

    Match for the first two letters of first name

--- MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
+++ MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
@@ -13,15 +13,36 @@ namespace MatchesAlgorithm
             var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
             CollectionAssert.AreEqual(new[] { "Ovidiu Jurje" }, UserNameMatches(userNameList, "O"));
         }
+       
+        [TestMethod]
+        public void FirstTwoLettersOfFirstNameTest()
+        {
+            var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
+            CollectionAssert.AreEqual(new[] { "Razvan Tamas", "Razvan Hidan" },UserNameMatches(userNameList,"Ra"));
+        }
 
         List<string> UserNameMatches(List<string> userNameList, string searchLetters)
         {
             var nameResults = new List<string>();
             foreach (var userName in userNameList)
             {
-                if (userName[0] == searchLetters[0]) nameResults.Add(userName);
+                MatchLettersInName(searchLetters, nameResults, userName);
             }
             return nameResults;
+        }
+
+        private static void MatchLettersInName(string searchLetters, List<string> nameResults, string userName)
+        {
+            int i = 0;
+            while (i < searchLetters.Length)
+            {
+                if (searchLetters[i] == userName[i])
+                {
+                    if (i == searchLetters.Length - 1) nameResults.Add(userName);
+                    i++;
+                }
+                else break;
+            }
         }
     }
 }
