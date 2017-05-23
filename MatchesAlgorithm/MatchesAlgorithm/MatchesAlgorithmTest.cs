be9cf98b1362a74ce70e7ddfffc38d55eb302d1b
TamasR <razvit88@gmail.com>
2017-05-23 00:54:29 -0700

    started new project MatchesAlgorithm

---
+++ MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MatchesAlgorithm
+{
+    [TestClass]
+    public class MatchesAlgorithmTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ MatchesAlgorithm/MatchesAlgorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MatchesAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MatchesAlgorithm")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f4db6f3-0706-429b-8f2c-c8f2d69654dd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
