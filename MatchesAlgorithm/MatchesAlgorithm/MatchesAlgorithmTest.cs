8360f5eefee80199a9677d0a7921b15208d618e1
TamasR <razvit88@gmail.com>
2017-05-23 23:52:44 -0700

    Added NameAndIndex structure and OrderSequenceOfIndexes

--- MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
+++ MatchesAlgorithm/MatchesAlgorithm/MatchesAlgorithmTest.cs
@@ -3,7 +3,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
 namespace MatchesAlgorithm
-{
+{   
+     public struct NameAndIndex
+    {
+        public string name;
+        public string index;
+        public NameAndIndex(string name,string index)
+        {
+            this.name = name;
+            this.index = index;
+        }
+    }
     [TestClass]
     public class MatchesAlgorithmTest
     {
@@ -45,47 +55,103 @@ namespace MatchesAlgorithm
             CollectionAssert.AreEqual(new[] { "Ovidiu Jurje" }, FindUsersNamesWithMatchingLetters(userNameList, "Ju"));
         }
         [TestMethod]
-        public void TestFor()
+        public void TestForMatchingIndexes()
         {
-
+            var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
+            var nameAndIndexTest = FindMatchingNamesAndIndexes(userNameList, "RazvTa");
+            Assert.AreEqual(nameAndIndexTest[0].index, "0-3,7-8");
+            
+        }
+        [TestMethod]
+        public void TestForMatchingNames()
+        {
+            var userNameList = new List<string> { "Razvan Tamas", "Ovidiu Jurje", "Razvan Hidan" };
+            var nameAndIndexTest = FindMatchingNamesAndIndexes(userNameList, "Hid");
+            Assert.AreEqual(nameAndIndexTest[0].name, "Razvan Hidan");
         }
        
         List<string> FindUsersNamesWithMatchingLetters(List<string> userNameList, string searchLetters)
         {
-            var nameResults = new List<string>();
+            var nameResults = new List<string>();          
             foreach (var userName in userNameList)
             {
-                if (IsMatching(searchLetters, userName))
+                var matchingIndex=new int[searchLetters.Length];
+                if (IsMatching(searchLetters, userName,ref matchingIndex))
                     nameResults.Add(userName);
             }
             return nameResults;
         }
 
-        public bool IsMatching(string searchLetters, string text)
+        NameAndIndex[] FindMatchingNamesAndIndexes(List<string> userNameList, string searchLetters)
+        {
+            var nameWithIndex = new NameAndIndex[] { };
+            string orderedSequence = string.Empty;
+            foreach (var userName in userNameList)
+            {
+                var matchingIndex = new int[searchLetters.Length];
+                if (IsMatching(searchLetters, userName, ref matchingIndex))
+                {
+                    Array.Resize(ref nameWithIndex, nameWithIndex.Length + 1);
+                    nameWithIndex[nameWithIndex.Length-1].name = userName;
+                    orderedSequence = OrderSequenceOfIndexes(matchingIndex);
+                    nameWithIndex[nameWithIndex.Length-1].index = orderedSequence;
+                }
+                   
+            }
+            return nameWithIndex;
+        }
+
+        public bool IsMatching(string searchLetters, string text, ref int[] matchingIndex)
         {
             var parts = text.Split(' ');           
             int i = 0;
             int j = 0;
-            int partIndex = 0;
+            int differenceBetweenIndexes = 0;
+            int partIndex = 0;           
             while (i < searchLetters.Length)
             {
+                
                 if (searchLetters[i] == parts[partIndex][j])
                 {
+                    matchingIndex[i] =j + differenceBetweenIndexes;
                     if (i == searchLetters.Length - 1)
-                    {
+                    {                       
                         return true;
-                    }                    
+                    }                  
                     i++;
-                    j++;
+                    j++;                  
                 }
-                else if (partIndex + 1 < parts.Length)
+                else if (partIndex + 1 < parts.Length && i < searchLetters.Length-1)
                 {
                     partIndex++;
+                    differenceBetweenIndexes += parts[partIndex - 1].Length + partIndex; 
                     j = 0;                  
                 }
                 else break;
             }
             return false;
+        }
+        public string OrderSequenceOfIndexes(int[] numbers)
+        {
+            string orderedSequence = string.Empty;
+            int start, end;
+            start = end = numbers[0];          
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1] + 1) end = numbers[i];
+                else
+                {
+                    if (start == end) orderedSequence += start + ",";
+                    else
+                    {
+                        orderedSequence += start + "-" + end + ",";
+                        start = end = numbers[i];
+                    }
+                }                               
+            }
+            if (start == end) orderedSequence += start;
+            else orderedSequence += start + "-" + end;
+            return orderedSequence;
         }
 
        
