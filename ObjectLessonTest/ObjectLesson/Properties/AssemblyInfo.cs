05e07be5178d91aff6e55a1e23ed40131c7d960e
TamasR <razvit88@gmail.com>
2017-05-31 13:37:31 +0300

    Started new project Student Registry with encapsulation and classes

---
+++ ObjectLessonTest/ObjectLesson/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ObjectLesson")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ObjectLesson")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c8136be-678d-4132-af4c-2d4a747c22ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ObjectLessonTest/ObjectLesson/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ObjectLesson
+{
+    class Student
+    {
+        private string name;
+        private Subject math;
+        private Subject english;
+        private Subject biology;
+        public Student(string name, Subject math, Subject english, Subject biology)
+        {
+            this.name = name;
+            this.math = math;
+            this.english = english;
+            this.biology = biology;
+        }
+        public string Name() 
+        {
+             return name;
+        }
+        public Subject Math()
+        {
+            return math;
+        }
+        public Subject English()
+        {
+            return english;
+        }
+        public Subject Biology()
+        {
+            return biology;
+        }
+       
+               
+    }
+    class Subject
+    {
+        private int[] grades;
+        public Subject(int[] grades)
+        {
+            this.grades = grades;
+        }
+        public decimal GetAverageGrade()
+        {
+            var sum = 0;
+            for(int i = 0; i < grades.Length; i++)
+            {
+                sum += grades[i];
+            }
+            return sum / grades.Length;
+        }
+    }
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(AverageGrade(), 9);
+        }
+        decimal AverageGrade()
+        {
+            Student oneStudent = new Student("razvan", new Subject(new int[] { 10, 7, 10 }), new Subject(new int[] { 9, 8, 10 }), new Subject(new int[] { 7, 8, 5 }));
+            var averageGrade = oneStudent.Math().GetAverageGrade();
+            return averageGrade;
+            
+            
+        }
+    }
+}
