48c27b8c11c5399c0f9933d1a3369ffa1b847a6d
TamasR <razvit88@gmail.com>
2017-06-16 09:35:38 +0300

    Started new project LinkedList

---
+++ LinkedList/LinkedList/LinkedListTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace LinkedList
+{   
+    public class LinkedListTest
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LinkedList")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinkedList")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e3d5c194-f5e9-4c31-abb2-5389d70e9682")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
